2e13c2c1b87da8e720429d8608226f28e7240fbb
ErikNoda3 <erik.noda@outlook.com>
2026-01-21 18:00:57 -0300

    Backend Node.js com API REST integrada ao front

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,13 +1,49 @@
 'use client'
 
 import { useRouter, useSearchParams } from "next/navigation"
+import { useState, useEffect } from "react"
 
 export default function Result() {
   const router = useRouter()
+
   const searchParams = useSearchParams()
   const prompt = searchParams.get("prompt")
 
-  const aiResponse = "Essa é uma resposta simulada por IA"
+  const [aiResponse, setAiResponse] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!prompt) return;
+
+    async function fetchAnalysis() {
+      try {
+        setLoading(true)
+        setError(null);
+        const res = await fetch("http://localhost:3333/analyze", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt })
+        })
+
+        if (!res.ok) {
+          throw new Error("Erro ao chamar API");
+        }
+
+        const data = await res.json();
+        setAiResponse(data.result ?? JSON.stringify(data))
+      } catch (err) {
+        setError("Erro ao processar requisição")
+
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchAnalysis()
+  }, [prompt])
+
 
   function handleBack() {
     router.push("/")
@@ -19,12 +55,24 @@ export default function Result() {
         <h1 className="text-5xl font-bold">CHAT</h1>
 
         <div className="flex gap-1">
-          <p className="text-2xl">Você: </p>
-          <p className="text-2xl">{prompt ?? "Nenhuma mensagem enviada"}</p>
+          <p className="text-2xl font-bold ">Você: </p>
+          <p className="text-2xl">{prompt}</p>
         </div>
 
+        {loading && (
+          <p>
+            Analisando texto...
+          </p>
+        )}
+
+        {error && (
+          <p>
+            {error}
+          </p>
+        )}
+
         <div className="flex gap-1">
-          <p className="text-2xl">IA: </p>
+          <p className="text-2xl font-bold">IA: </p>
           <p className="text-2xl">{aiResponse}</p>
         </div>
         <button onClick={handleBack} className="p-2 font-bold text-white bg-emerald-400 rounded">Voltar</button>
